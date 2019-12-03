8ebfcda39dd5f2fe46e8f6483c4980642e9fc173
Ben Ramadhan <54801570+WaxmanProduction@users.noreply.github.com>
2019-12-03 15:47:01 +0000

    Updated Space Invaders
    Space Invaders, bullets now working and movement works. Need to update enemy movement; massive overhaul

--- Programming-Techniques/Space_Invaders.py
+++ Programming-Techniques/Space_Invaders.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import math
 
-# -- Colours
+# -- Colours -- #
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 BLUE = (50,50,255)
@@ -10,80 +10,131 @@ YELLOW = (255,255,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 
-# -- Initialise PyGame
+# -- Initialise PyGame -- #
 pygame.init()
 
-# -- Manages screen refresh
+# -- Manages screen refresh -- #
 clock = pygame.time.Clock()
 
-# -- Sets screen
+# -- Sets screen -- #
 size = (640,480)
 screen = pygame.display.set_mode(size)
 
-# -- Window title
+# -- Window title -- #
 pygame.display.set_caption("Space Invaders")
 
 
+# -- Fonts == #
+basicfont = pygame.font.SysFont(None, 36)
+
+# -- Classes -- #
+
+# -- Creates invaders
 class Invader(pygame.sprite.Sprite):
-    def __init__(self,color,width,height,speed):
+    def __init__(self,colour,width,height,speed):
         super().__init__()
         self.image = pygame.Surface([width,height])
-        self.image.fill(color)
+        self.image.fill(colour)
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(0,600)
+        self.rect.x = random.randrange(10,630)
+
         self.rect.y = random.randrange(-50,0)
         self.speed = speed
+    #End Public Procedure
 
     def update(self):
         self.rect.y += self.speed
+    #End Public Procedure
 
+# EndClass
+
+# -- Creates player
 class Player(pygame.sprite.Sprite):
-    def __init__(self,color,width,height):
+    def __init__(self,colour,width,height):
         super().__init__()
         self.image = pygame.Surface([width,height])
-        self.image.fill(color)
+        self.image.fill(colour)
         self.rect = self.image.get_rect()
         self.rect.x = 300
         self.rect.y = 450
         self.speed = 0
-    #End Public Procedure (constructor)
+    #End Public Procedure
 
     def update(self):
-       self.rect.y += self.speed
-    #End Public Procedure
-       
-    def player_set_speed(val):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                Player.player_set_speed(-3)
-            elif event.key == pygame.K_RIGHT:
-               Player.player_set_speed(3) 
-        elif event.type == pygame.KEYUP: 
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                Player.player_set_speed(0)
-   
+       self.rect.x += self.speed
     #End Public Procedure
 
+    def player_set_speed(self,val):
+        speed = val
+        if speed == -3:
+            if self.rect.x > 620:
+                self.speed = -3
+            else:
+                self.speed += -3
+        elif speed == 3:
+            if self.rect.x < 0:
+                self.speed = +3
+            else:
+                self.speed += 3
+        elif speed == 0:
+            self.speed = 0
+    #End Public Procedure
+
+
+        
 #EndClass
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,colour,width,height):
+        super().__init__()
+        self.image = pygame.Surface([width,height])
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+    #End Public Procedure
 
+    def update(self):
+        self.rect.y -= 5
+    #End Public Procedure
+
+class Invader_bullet(pygame.sprite.Sprite):
+    def __init__(self,colour,width,height):
+        super().__init__()
+        self.image = pygame.Surface([width,height])
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+    #End Public Procedure
+
+    def update(self):
+        self.rect.y += 5
+    #End Public Procedure
+        
+score = 0     
+lives = 5
+bullet_count = 50
+    
 
 game_over = False
+
+bullet_group = pygame.sprite.Group()
 invader_group = pygame.sprite.Group()
-my_player = Player(RED, 20, 10)
+invader_attacks = pygame.sprite.Group()
+hero = Player(RED, 20, 10)
+
 
 all_sprites_group = pygame.sprite.Group()
 
-all_sprites_group.add(my_player)
+all_sprites_group.add(hero, invader_attacks)
 
-flakes_num = 15
-for x in range (flakes_num):
-    my_invader = Invader(WHITE, 10, 10, 1)
-    invader_group.add(my_invader)
-    all_sprites_group.add(my_invader)
+
+for _ in range (60):
+    invader = Invader(WHITE, 10, 10, 1)
+    invader_group.add(invader)
+    all_sprites_group.add(invader)
     
 
 clock = pygame.time.Clock()
+
+### -- Game Loop -- ###
 
 while not game_over:
     for event in pygame.event.get():
@@ -91,19 +142,50 @@ while not game_over:
             game_over = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                Player.player_set_speed(-3)
+                Player.player_set_speed(hero,-3)
             elif event.key == pygame.K_RIGHT:
-               Player.player_set_speed(3) 
+               Player.player_set_speed(hero,3)
+            elif event.key == pygame.K_UP and bullet_count != 0:
+                bullet = Bullet(RED,5,10)
+                bullet.rect.x = hero.rect.x + 7
+                bullet.rect.y = hero.rect.y
+                all_sprites_group.add(bullet)
+                bullet_group.add(bullet)
+                bullet_count -= 1
         elif event.type == pygame.KEYUP: 
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                Player.player_set_speed(0)
-   
+                Player.player_set_speed(hero,0)
+
+    # == Counters
+    score_counter = basicfont.render("Score = " + str(score), True, WHITE)
+    bullet_counter = basicfont.render("Bullets Remaining = " + str(bullet_count), True, WHITE)
+    life_counter = basicfont.render(str(lives) + " lives", True, WHITE)
+
     all_sprites_group.update()
+
+    for bullet in bullet_group:
+        if pygame.sprite.spritecollide(bullet, invader_group, True):
+            bullet_group.remove(bullet)
+            all_sprites_group.remove(bullet)
+            score += 1
+        elif bullet.rect.y < -10:
+            bullet_group.remove(bullet)
+            all_sprites_group.remove(bullet)
+
+    player_hit_group = pygame.sprite.spritecollide(hero, invader_group, True)
     
-    
+    # -- Background
     screen.fill(BLACK)
+    screen.blit(score_counter, (20,20))
+    screen.blit(bullet_counter, (20,45))
+    screen.blit(life_counter, (20,70))
     
     all_sprites_group.draw(screen)
+
+    for foo in player_hit_group:
+        lives -= 1
+        all_sprites_group.remove(hero)
+        bullet_count = 0
     
     pygame.display.flip()
     clock.tick(60)
